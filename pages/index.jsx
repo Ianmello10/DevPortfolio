30ae165b79ded541a0949dfd34c50b206f232d1f
lucas <lucasian.azul@gmail.com>
2022-12-31 00:36:22 -0300

    chages

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>PortfolioSite</title>
+        <title>MeuPortfolioDev</title>
         <meta name="description" content="Site portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="" />
