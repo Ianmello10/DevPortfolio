d237f48a8c0310851dfc3360fdf0ec5f4b755ad2
Ianmello10 <lucasian.azul@gmail.com>
2023-01-16 22:37:00 -0300

    changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MeuPortfolioDev</title>
+        <title>MeuPortfolioDeev</title>
         <meta name="description" content="Site portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="" />
