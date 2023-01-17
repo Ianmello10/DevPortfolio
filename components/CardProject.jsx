c580bb6bc9068c1a31aff7e82d0915f2a6183760
Ianmello10 <lucasian.azul@gmail.com>
2023-01-16 23:28:35 -0300

    changes

--- components/CardProject.jsx
+++ components/CardProject.jsx
@@ -4,31 +4,33 @@ import Image from "next/image";
 function CardProject({ ProjetoName, img, tag1, tag2, tag3, descricao, link }) {
   return (
     <>
-      <div className="  flex flex-col md:w-[70%] md:mx-auto lg:mx-0 lg:flex-row lg:w-full   mt-10">
-        <div className="w-full lg:w-[50%] h-[200px]">
-          <Image className="w-full h-full" src={img} alt="imagem projeto"/>
+      <div className="w-full  flex flex-col md:w-[70%] md:mx-auto lg:mx-0 lg:flex-row lg:w-full  mb-14 mt-10">
+        <div className="w-full lg:w-[50%] h-[240px]">
+          <Image className="w-full h-full" src={img} alt="imagem projeto" />
         </div>
 
-        <div className="flex gap-2  flex-col mt-2 lg:mt-0">
-          <div className="flex flex-col  items-center gap-x-10 w-full    h-12 px-8 lg:flex-row">
-            <h1 className="text-2xl mt-2 lg:mt-0">{ProjetoName}</h1>
+        <div className="w-full h-auto flex gap-2  flex-col mt-2 lg:mt-0 ">
+          <h1 className="text-2xl text-center mt-4   lg:text-left lg:mt-2  px-8">
+            {ProjetoName}
+          </h1>
 
-            <div className="flex mt-4 w-full  justify-around   text-sm lg:w-[40%] lg:mt-0">
-              <span className="border-2 border-gray-600 h-6 w-[80px] lg:w-[100px] flex items-center justify-center  rounded-md">
-                {tag1}
-              </span>
-              <span className="border-2 border-gray-600 h-6  w-[80px]  lg:w-[100px] flex items-center justify-center rounded-md">
-                {tag2}
-              </span>
-              <span className="border-2 border-gray-600 h-6 w-[80px] lg:w-[100px] flex items-center justify-center rounded-md">
-                {tag3}
-              </span>
-            </div>
+          <div className="flex  mt-4 w-full  justify-around  lg:justify-between text-sm lg:w-[60%] lg:px-8   lg:mt-2 ">
+            <span className="border-2 border-gray-600 h-6 w-auto lg:w-auto px-2 flex items-center justify-center  rounded-md">
+              {tag1}
+            </span>
+            <span className="border-2 border-gray-600 h-6  w-auto  lg:w-auto px-2 flex items-center justify-center rounded-md">
+              {tag2}
+            </span>
+            <span className="border-2 border-gray-600 h-6 w-auto lg:w-auto px-2 flex items-center justify-center rounded-md">
+              {tag3}
+            </span>
           </div>
 
-          <div className="mt-8 lg:px-8 lg:mt-0">
+          <div className="mt-4 lg:px-8 lg:mt-0">
             <p className="mt-2 lg:mt-0">{descricao}</p>
-            <a target='_blank' href={link} className="mt-4 block lg:mt-2">Ver projeto</a>
+            <a target="_blank" href={link} className="mt-4 block lg:mt-2">
+              Ver projeto
+            </a>
           </div>
         </div>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import avatar from "../public/avatar.svg";
 import coindesk from "../public/coindesk.png";
 import todoreact from "../public/todoreact.png";
 import calcjs from "../public/calcjs.png";
+import productPage from "../public/productPage.png";
 import Image from "next/image";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
@@ -15,12 +16,11 @@ import { IoLogoHtml5 } from "react-icons/io";
 import { GrReactjs } from "react-icons/gr";
 import CardProject from "../components/CardProject";
 
-
 export default function Home() {
   return (
     <>
       <Head>
-        <title>MeuPortfolioDeev</title>
+        <title>MeuPortfolioDev</title>
         <meta name="description" content="Site portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="" />
@@ -28,10 +28,10 @@ export default function Home() {
 
       <NavBar />
 
-      <section className="w-[90%] mx-auto px-2 mt-14  font-primary  lg:mb-36 :py-2"  >
+      <section className="w-[90%] mx-auto px-2 mt-14  font-primary  lg:mb-36 :py-2">
         <div className="w-full flex flex-col  lg:flex-row h-auto lg:justify-between   ">
-          <div className="w-full lg:w-[60%] mt-4  ">
-            <h1 className="text-3xl mt-4 text-black dark:text-cyan-600  " >
+          <div className="w-full lg:w-[60%] mt-0  ">
+            <h1 className="text-3xl mt-0 text-black dark:text-cyan-600  ">
               Olá , eu sou o Lucas &#128075;
             </h1>
 
@@ -47,12 +47,19 @@ export default function Home() {
             </p>
 
             <div className="flex w-28 mt-8 justify-between h-4  ">
-              <a className="lg:hover:-mt-2 transition-[margin] ease-in " target='_blank' href="https://github.com/Ianmello10">  
-              <AiFillGithub className="w-10 h-10" />
-              </a> 
-              <a className="lg:hover:-mt-2 transition-[margin] ease-in " target='_blank' href="https://www.linkedin.com/in/lucas-ian-b62195225/">
-              <AiFillLinkedin className="w-10 h-10" />
-                 
+              <a
+                className="lg:hover:-mt-2 transition-[margin] ease-in "
+                target="_blank"
+                href="https://github.com/Ianmello10"
+              >
+                <AiFillGithub className="w-10 h-10" />
+              </a>
+              <a
+                className="lg:hover:-mt-2 transition-[margin] ease-in "
+                target="_blank"
+                href="https://www.linkedin.com/in/lucas-ian-b62195225/"
+              >
+                <AiFillLinkedin className="w-10 h-10" />
               </a>
             </div>
           </div>
@@ -69,8 +76,10 @@ export default function Home() {
         </h2>
 
         <div className="w-full flex flex-col justify-between mt-6 lg:flex-row">
-          <div className=" w-[90%] md:w-[70%] mx-auto mb-6 lg:w-[23%] border-2
-           border-gray-600 rounded-md  h-42 py-2 px-2">
+          <div
+            className=" w-[90%] md:w-[70%] mx-auto mb-6 lg:w-[23%] border-2
+           border-gray-600 rounded-md  h-42 py-2 px-2"
+          >
             <h2 className="text-xl">HTML 5</h2>
             <p className="text-sm">
               HTML (Hypertext Markup Language) é uma linguagem de marcação
@@ -122,7 +131,25 @@ export default function Home() {
       </section>
 
       <section className="w-[90%] mx-auto py-4 mt-24 ">
-        <h2 id="projetos" className="text-3xl pl-1 md:text-center lg:text-left lg:px-0">Projetos Recentes</h2>
+        <h2
+          id="projetos"
+          className="text-3xl pl-1 md:text-center lg:text-left lg:px-6"
+        >
+          Projetos Recentes
+        </h2>
+
+        <CardProject
+          ProjetoName="Página de produto"
+          img={productPage}
+          tag1={"React"}
+          tag2={"ContextApi"}
+          tag3={"StyledComponents"}
+          descricao={`O objetivo era reforçar os conhecimentos em react js e css.Usei o
+           contextApi para armazenar o número de itens no carrinho e fazer o compartilhamento de estado entre os componentes
+           como adicionar, remover e calcular o preço total da soma dos produtos.Também usei o Styled
+            Components para estilizar a página e aprender um pouco mais sobre essa ferrmenta. `}
+          link={"https://product-page-dusky.vercel.app/"}
+        />
 
         <CardProject
           ProjetoName="Coindesk"
@@ -135,7 +162,7 @@ export default function Home() {
              utilizei o Zustand para gerenciar o estado da aplicação.
            O objetivo era criar uma interface de usuário fácil de usar para acompanhar 
            o mercado de criptomoedas. `}
-           link={'https://coindesk-omega.vercel.app/'}
+          link={"https://coindesk-omega.vercel.app/"}
         />
 
         <CardProject
@@ -149,8 +176,7 @@ export default function Home() {
           marcá-las como concluídas.Para criar a interface de usuário, utilizei o Tailwind CSS 
           como framework de estilo, que me permitiu criar um design limpo e minimalista
            com pouco código CSS. `}
-           link={'https://todo-react-neon.vercel.app/'}
-
+          link={"https://todo-react-neon.vercel.app/"}
         />
 
         <CardProject
@@ -161,7 +187,7 @@ export default function Home() {
           tag3={"JavaScript"}
           descricao={`O projeto consiste em uma calculadora simples feita com JavaScript, HTML e CSS que permite aos usuários realizar cálculos básicos. A interface de usuário foi criada com HTML e CSS e a lógica de cálculo foi implementada com JavaScript.
            O objetivo era colocar em prática conhecimentos adquiridos`}
-           link={'https://ianmello10.github.io/Calculator-app/'}
+          link={"https://ianmello10.github.io/Calculator-app/"}
         />
       </section>
 
